3de2c27fc1531ebd97747a5e487ce871e50debc8
MostafaDawi <mostafadawi@hotmail.com>
2025-07-24 21:14:44 +0300

    feat: Implemented user authentication module

---
+++ backend/user_service/app/auth/core/token.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from app.auth.core import config
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now() + (expires_delta or timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
