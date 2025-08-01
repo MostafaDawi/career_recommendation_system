a03235d2006aff7fc300e8bbbd7039530d8b8c46
MostafaDawi <mostafadawi@hotmail.com>
2025-08-01 21:17:08 +0300

    Refactoring done for Home, MBTI and Results pages!

---
+++ frontend/app/src/components/AppWrapper.tsx
@@ -0,0 +1,58 @@
+import { Routes, Route, useNavigate } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.js";
+import AuthLayout from "../layouts/AuthLayout.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import MBTIQuiz from "../pages/MBTIQuiz.js";
+import { useState } from "react";
+import ResultsPage from "../pages/ResultsPage.js";
+import HomePage from "../pages/HomePage.js";
+
+function AppWrapper() {
+  const [page, setPage] = useState("home"); // 'home', 'quiz', 'results'
+  const [mbtiType, setMbtiType] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const handleStartQuiz = () => {
+    navigate("/quiz");
+  };
+
+  const handleQuizComplete = (type: string) => {
+    setMbtiType(type);
+    navigate("/results");
+  };
+
+  const handleRestart = () => {
+    setMbtiType(null);
+    navigate("/");
+  };
+
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route
+            path="/quiz"
+            element={<MBTIQuiz onComplete={handleQuizComplete} />}
+          />
+          <Route
+            path="/results"
+            element={<ResultsPage type={mbtiType} onRestart={handleRestart} />}
+          />
+          <Route
+            path="/"
+            element={<HomePage onStartQuiz={handleStartQuiz} />}
+          />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        {/* Add more routes here */}
+      </Routes>
+    </>
+  );
+}
+
+export default AppWrapper;
--- frontend/app/src/layouts/MainLayout.tsx
+++ frontend/app/src/layouts/MainLayout.tsx
@@ -4,12 +4,15 @@ import Footer from "../components/Footer";
 import MBTIQuiz from "../pages/MBTIQuiz.js";
 import ResultsPage from "../pages/ResultsPage.js";
 import HomePage from "../pages/HomePage.js";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
-      <main className="bg-gray-50 min-h-screen">{renderPage()}</main>
+      <main className="bg-gray-50 min-h-screen">
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
