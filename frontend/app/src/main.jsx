66215b210c95db97e70eb27596a9bb5729ef7370
zahraa meselmani <zahraameselamni0@gmail.com>
2025-08-10 22:51:41 +0300

    Added file for API link in recommendation page

--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router } from "react-router-dom";  // <-- استيراد الراوتر
 import App from "./App.jsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <Router> {/* <-- هنا نلف كل التطبيق */}
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
 );
+
