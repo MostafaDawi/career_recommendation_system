868b0c8bf1d0b76a2b3d98cc5824edf756a4be56
MostafaDawi <mostafadawi@hotmail.com>
2025-08-14 20:10:25 +0300

    Modified API Gateway URL in frontend

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import AppWrapper from "./components/AppWrapper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+console.log("URL: ", import.meta.env.VITE_API_GATEWAY_URL);
+
 const queryClient = new QueryClient();
 
 function App() {
